1d3a8afffd7ac7a963016c2f5ea6d807a990a174
danilofernandes1987 <danilofernandes1987@gmail.com>
2021-07-29 13:21:28 -0300

    Avanço das aulas

--- src/models/Login.php
+++ src/models/Login.php
@@ -1,16 +1,21 @@
 <?php
 
-require_once(realpath(MODEL_PATH . '/User.php'));
+// require_once(realpath(MODEL_PATH . '/User.php'));
+loadModel('User');
 
 class Login extends Model
 {
     public function checkLogin(){
         $user = User::getOne(['email' => $this->email]);
         if($user){
+            if($user->end_date){
+                throw new AppException('Usuário esta desligado da empresa.');
+
+            }
             if(password_verify($this->password, $user->password)){
                 return $user;
             }
         }
-        throw new Exception();
+        throw new AppException('Usuário e Senha inválidos.');
     }
 }
